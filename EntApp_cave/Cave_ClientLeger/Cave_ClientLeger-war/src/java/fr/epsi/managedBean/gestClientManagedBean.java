57e75f72974e67f4a4d5f55b8e8e20a4b99661b9
Anthony Sanchez <anthony@happster.fr>
2013-06-03 17:22:25 +0200

    Add Client Managed Beans

---
+++ EntApp_cave/Cave_ClientLeger/Cave_ClientLeger-war/src/java/fr/epsi/managedBean/gestClientManagedBean.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.epsi.managedBean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author Antho
+ */
+@ManagedBean
+@SessionScoped
+public class gestClientManagedBean {
+
+    /**
+     * Creates a new instance of gestClientManagedBean
+     */
+    public gestClientManagedBean() {
+    }
+}
